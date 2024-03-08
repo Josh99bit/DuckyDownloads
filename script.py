961feb711be4d643ad8e8f178a35b6d94672d0c3
Josh99bit <52200922+Josh99bit@users.noreply.github.com>
2024-03-08 15:12:41 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,32 @@
-import os
 try:
-    import pyautogui
+    import pygame
 except:
-    os.system("pip install pyautogui")
-    import pyautogui
+    import os
+    os.system("pip install pygame") 
+    import pygame   
 import random
-import time
+
+
+pygame.init()
+
+infoObject = pygame.display.Info()
+screen_width = infoObject.current_w
+screen_height = infoObject.current_h
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Python Prank!")
+
+font = pygame.font.SysFont("Arial", 64)
 
 while True:
-    # Move the mouse cursor randomly
-    x_offset = random.randint(-250, 250)
-    y_offset = random.randint(-250, 250)
-    pyautogui.moveRel(x_offset, y_offset, duration=0.25)
-    time.sleep(0.1)  # 1 second delay
+
+    text_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    text = font.render(":-) RUBBER DUCKS WILL TAKE OVER THE OCEANS! (-: )", True, text_color)
+
+    x_offset = random.randint(-50, 50)
+    y_offset = random.randint(-50, 50)
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width//2 + x_offset, screen_height//2 + y_offset)
+
+    screen.fill((0, 0, 0))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
